10850990e5dfd1c96b952db508333d43fa72c4b8
Thiago Meira <thiago_meira@hotmail.com>
2024-08-07 03:14:05 -0300

    Mudanças e refatoração de código

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import app from './src/app.ts';
+const app = require('./dist/app.js').default;
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${3001}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
---
+++ setupTests.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+const MONGO_URI = 'mongodb://localhost:27017/your_database_name';
+beforeAll(async () => {
+    await mongoose.connect(MONGO_URI);
+});
+afterEach(async () => {
+    await mongoose.connection.dropDatabase();
+});
+afterAll(async () => {
+    await mongoose.connection.close();
+});
